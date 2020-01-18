60d73944ce70e89675e5249d6c153eced289c8ac
santiagoWilli <santiago30mw@gmail.com>
2020-01-18 20:37:59 +0000

    lil ref

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,17 +1,15 @@
 public class FizzBuzz {
-    public FizzBuzz() {
-    }
 
     String of(int i) {
         if (isDivisibleByThree(i)) return "Fizz";
         return (isDibisibleByFive(i) ? "Buzz" : String.valueOf(i));
     }
 
-    boolean isDivisibleByThree(int i) {
+    private boolean isDivisibleByThree(int i) {
         return i % 3 == 0;
     }
 
-    boolean isDibisibleByFive(int i) {
+    private boolean isDibisibleByFive(int i) {
         return i % 5 == 0;
     }
 }
