8e6101e0f62320046159b2df07bdbacedb1d5a0f
santiagoWilli <santiago30mw@gmail.com>
2020-01-18 19:57:14 +0000

    one_should_return_1

---
+++ src/test/java/FizzBuzz_.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FizzBuzz_ {
+    @Test
+    public void one_should_return_1() {
+        assertThat(of(1)).isEqualTo("1");
+    }
+
+    private String of(int i) {
+        return String.valueOf(i);
+    }
+}
