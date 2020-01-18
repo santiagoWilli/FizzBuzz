2d694a07f3f9cde25b7cc5b8cc9a88bda509fbc5
santiagoWilli <santiago30mw@gmail.com>
2020-01-18 20:17:13 +0000

    six_should_return_Fizz

--- src/test/java/FizzBuzz_.java
+++ src/test/java/FizzBuzz_.java
@@ -20,8 +20,13 @@ public class FizzBuzz_ {
         assertThat(of(5)).isEqualTo("Buzz");
     }
 
+    @Test
+    public void six_should_return_Fizz() {
+        assertThat(of(6)).isEqualTo("Fizz");
+    }
+
     private String of(int i) {
-        if (i != 3 && i != 5) return String.valueOf(i);
-        return (i == 3 ? "Fizz" : "Buzz");
+        if (i == 3 || i == 6) return "Fizz";
+        return (i == 5 ? "Buzz" : String.valueOf(i));
     }
 }
